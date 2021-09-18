850d234ef60793be6cc6eaea78269499f75423d3
Paul Scott <32460795+LijahTheDev@users.noreply.github.com>
2021-09-18 09:31:56 -0500

    Added Quiz History

---
+++ src/js/history.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { docQuery } from "./helpers";
+import { starRatingImageUrls } from './helpers'
+import { User } from "./user";
+import humanize from 'humanize-duration'
+import day from 'dayjs'
+
+class Quizzes {
+    public static async getQuizzes() {
+        try {
+            const userId = window.localStorage.getItem('userId')
+            const result = await axios.get(`http://localhost:5000/api/quiz/user/${userId}`)
+            const quizzes = result.data
+
+            console.log(quizzes)
+
+            const quizListSection = docQuery('.quiz-list-section')
+
+            quizListSection.innerHTML = this.buildQuizList(quizzes)
+
+            User.loadUserStars()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    private static buildQuizList(quizzes) {
+        return quizzes
+        .sort((a,b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+        .map(q => (
+            `<div class="quiz-block">
+            <img src="${starRatingImageUrls[q.star_level]}" class="stars" width="50px" />
+            <div class="quiz-card">
+                <h2><span>${q.word.text}</span></h2>
+                <p>${humanize(q.elapsed_time)}</p>
+                <p>${day(q.created_at).format('MMM D, YYYY h:mm A (Z)')}</p>
+            </div>
+        </div>`
+        )).join('')
+    }
+}
+
+Quizzes.getQuizzes()
